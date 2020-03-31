20f756b3a2f6ea1da3f23e412f0c6ed0d2100547
Magnus Olofsson <magnusolofsson@outlook.com>
2020-03-31 09:12:05 +0200

    Added color to ball on impact

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pypong
 a simple pong game in python, using pygame
+
+Starting from the instructions in this video: https://www.youtube.com/watch?v=Qf3-aDXG8q4
+and then trying to improve upon that, in order to improve my python skills.
--- pong.py
+++ pong.py
@@ -1,7 +1,16 @@
 import pygame, sys, random
 
+# General setup
+pygame.init()
+clock = pygame.time.Clock()
+
+ball_speed_x = 7
+ball_speed_y = 7
+player_speed = 0
+opponent_speed = 7
+
 def ball_animation():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, ball_color, ball_color_delay
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
@@ -12,6 +21,8 @@ def ball_animation():
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
+        ball_color=blue_ball
+        ball_color_delay=0
 
 def player_animation():
     player.y += player_speed
@@ -41,9 +52,6 @@ def exitgame():
     keeprunning=False
     
 
-# General setup
-pygame.init()
-clock = pygame.time.Clock()
 
 # Setting up the main window
 screen_width = 1280
@@ -59,11 +67,11 @@ opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 
 bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
+blue_ball = (0,0,255) #yeah i know... :)
+ball_color = light_grey
+ball_color_delay=0
 
-ball_speed_x = 7
-ball_speed_y = 7
-player_speed = 0
-opponent_speed = 7
+
 keeprunning = True
 while keeprunning:
     #Handling input
@@ -91,12 +99,18 @@ while keeprunning:
     screen.fill(bg_color)
     pygame.draw.rect(screen,light_grey, player)
     pygame.draw.rect(screen,light_grey, opponent)
-    pygame.draw.ellipse(screen, light_grey, ball)
+    pygame.draw.ellipse(screen, ball_color, ball)
     pygame.draw.aaline(screen, light_grey, (screen_width/2,0), (screen_width/2,screen_height))
 
     # Updating the window
     pygame.display.flip()
-
+    
+    # reset ball color every 10 cycles (if it's been blue it should become light grey again).
+    if ball_color != light_grey:
+        ball_color_delay += 1
+        if ball_color_delay == 10:
+            ball_color = light_grey
     clock.tick(60)
 
+# when the while loop ends, we will quit the game.
 pygame.quit()
